fda6a808db73a4207cfe5bb34f0219e4cb4c7700
oojas <oojasgupta@gmail.com>
2021-04-21 08:29:20 +0530

    Added provider package and started building the UI

---
+++ lib/design1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+// ignore: camel_case_types
+class d1 extends StatefulWidget {
+  @override
+  _d1State createState() => _d1State();
+}
+
+// ignore: camel_case_types
+class _d1State extends State<d1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
