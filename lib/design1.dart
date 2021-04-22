a74f78d97d60f4ce1569328649e76f184ebe5d23
oojas <oojasgupta@gmail.com>
2021-04-22 11:47:12 +0530

    UI building started

--- lib/design1.dart
+++ lib/design1.dart
@@ -11,6 +11,63 @@ class d1 extends StatefulWidget {
 class _d1State extends State<d1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: height / 3,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.yellowAccent,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(35),
+                        bottomRight: Radius.circular(35)),
+                  ),
+                ),
+                Positioned(
+                    top: 1,
+                    right: 1,
+                    child:
+                        IconButton(icon: Icon(Icons.search), onPressed: () {})),
+                Positioned(
+                    top: 1,
+                    left: 1,
+                    child:
+                        IconButton(icon: Icon(Icons.menu), onPressed: () {})),
+                Positioned(
+                  left: 100,
+                  top: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                              'https://image.freepik.com/free-vector/businessman-profile-cartoon_18591-58479.jpg')),
+                      SizedBox(width: 30),
+                      Text(
+                        'Ojas Gupta',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:stateanagement/design1.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: d1(),
-    );
-  }
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: d1(),
+  ));
 }
