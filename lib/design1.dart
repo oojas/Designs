06db79fe46cb33cac3b1ca1cd94f8d3ba671195c
oojas <oojasgupta@gmail.com>
2021-04-23 09:24:00 +0530

    Design completed

--- lib/design1.dart
+++ lib/design1.dart
@@ -23,7 +23,7 @@ class _d1State extends State<d1> {
             Stack(
               children: [
                 Container(
-                  height: height / 3,
+                  height: height / 3.5,
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.yellowAccent,
@@ -35,21 +35,29 @@ class _d1State extends State<d1> {
                 Positioned(
                     top: 1,
                     right: 1,
-                    child:
-                        IconButton(icon: Icon(Icons.search), onPressed: () {})),
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.search,
+                          size: 30,
+                        ),
+                        onPressed: () {})),
                 Positioned(
                     top: 1,
                     left: 1,
-                    child:
-                        IconButton(icon: Icon(Icons.menu), onPressed: () {})),
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.menu,
+                          size: 30,
+                        ),
+                        onPressed: () {})),
                 Positioned(
-                  left: 100,
+                  left: 60,
                   top: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       CircleAvatar(
-                          radius: 40,
+                          radius: 60,
                           backgroundImage: NetworkImage(
                               'https://image.freepik.com/free-vector/businessman-profile-cartoon_18591-58479.jpg')),
                       SizedBox(width: 30),
@@ -71,6 +79,127 @@ class _d1State extends State<d1> {
                     ],
                   ),
                 )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'My Tasks',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 26),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: NetworkImage(
+                        'https://www.freeiconspng.com/thumbs/tasks-icon/tasks-icon-14.png'),
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.red,
+                child: Text(
+                  'D',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              title: Text(
+                'To-Do List',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              subtitle: Text('What you need to do'),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.yellow,
+                child: Text(
+                  'P',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              title: Text(
+                'In Progress',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              subtitle: Text('3 Tasks are in progress'),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.blue,
+                  child: Icon(Icons.done_all_outlined)),
+              title: Text(
+                'Tasks Completed',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              subtitle: Text('4 tasks are finished'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Container(
+                    width: 190,
+                    height: 250,
+                    decoration: BoxDecoration(color: Colors.green[500]),
+                  ),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Container(
+                    width: 190,
+                    height: 250,
+                    decoration: BoxDecoration(color: Colors.red),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Container(
+                    width: 190,
+                    height: 250,
+                    decoration: BoxDecoration(color: Colors.yellow),
+                  ),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Container(
+                    width: 190,
+                    height: 250,
+                    decoration: BoxDecoration(color: Colors.lightBlue),
+                  ),
+                ),
               ],
             )
           ],
